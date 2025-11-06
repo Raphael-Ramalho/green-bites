656f52290029d86acb3a66d06729c0d265569c7f
Raphael De Souza Ramalho <rdesouzaramalho@apple.com>
2025-11-06 19:23:28 -0300

    update

---
+++ src/components/CategoryComponent.vue
@@ -0,0 +1,135 @@
+<template>
+  <div class="flex flex-col gap-10">
+    <div id="text-content" class="flex flex-col gap-2">
+      <h1 class="text-2xl md:text-4xl font-medium text-primary-green">Browse Our Category</h1>
+      <h1 class="text-2xl md:text-4xl font-medium">Receipt</h1>
+    </div>
+    <div class="relatives">
+      <!-- Carousel Wrapper -->
+      <div
+        ref="carousel"
+        class="relative flex overflow-x-auto snap-x snap-mandatory scroll-smooth space-x-4 w-screen scrollbar-hide"
+      >
+        <div
+          v-for="(item, index) in categories"
+          :key="index"
+          :class="[item.bgColor]"
+          class="w-[230px] h-[175px] flex-none snap-start bg-none rounded-lg flex flex-col py-auto justify-center items-center hover:min-h-[190px] hover:bg-[url(../assets/bg-main.png)] hover:bg-cover hover:bg-blend-soft-light"
+        >
+          <img :src="item.imgSrc" alt="Category" class="w-[47px] h-[52px] mb-2" />
+          <p class="font-medium">{{ item.name }}</p>
+          <p class="text-sm text-gray-500">{{ item.items }} Items</p>
+        </div>
+      </div>
+    </div>
+    <div id="nav-button" class="hidden md:flex md:gap-2 md:self-end">
+      <button
+        @click="prevSlide()"
+        class="flex cursor-pointer flex-row items-center gap-2 rounded-full bg-primary-green px-4 py-2"
+      >
+        <img src="../assets/icons/left-arrow.png" class="h-[33px] w-[33px] object-cover" alt="" />
+        <span class="text-white">PREV</span>
+      </button>
+      <button
+        @click="nextSlide()"
+        class="flex cursor-pointer flex-row items-center gap-2 rounded-full bg-primary-green px-4 py-2"
+      >
+        <span class="text-white">NEXT</span>
+        <img src="../assets/icons/right-arrow.png" class="h-[34px] w-[34px] object-cover" alt="" />
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import Cookie from '@/assets/icons/cookie.png'
+import Pizza from '@/assets/icons/pizza.png'
+import Kebab from '@/assets/icons/kebab.png'
+import Salmon from '@/assets/icons/salmon.png'
+import Doughnut from '@/assets/icons/doughnut.png'
+
+const carousel = ref(null)
+
+const categories = ref([
+  {
+    name: 'Cupcake',
+    items: 25,
+    bgColor: 'bg-soft-primary-green',
+    imgSrc: Cookie,
+  },
+  {
+    name: 'Pizza',
+    items: 23,
+    bgColor: 'bg-soft-primary-teal',
+    imgSrc: Pizza,
+  },
+  {
+    name: 'Kebab',
+    items: 12,
+    bgColor: 'bg-soft-primary-blue',
+    imgSrc: Kebab,
+  },
+  {
+    name: 'Salmon',
+    items: 22,
+    bgColor: 'bg-soft-primary-magenta',
+    imgSrc: Salmon,
+  },
+  {
+    name: 'Doughnut',
+    items: 15,
+    bgColor: 'bg-soft-primary-lightgreen',
+    imgSrc: Doughnut,
+  },
+  {
+    name: 'Cupcake',
+    items: 25,
+    bgColor: 'bg-soft-primary-green',
+    imgSrc: Cookie,
+  },
+  {
+    name: 'Pizza',
+    items: 23,
+    bgColor: 'bg-soft-primary-teal',
+    imgSrc: Pizza,
+  },
+  {
+    name: 'Kebab',
+    items: 12,
+    bgColor: 'bg-soft-primary-blue',
+    imgSrc: Kebab,
+  },
+  {
+    name: 'Salmon',
+    items: 22,
+    bgColor: 'bg-soft-primary-magenta',
+    imgSrc: Salmon,
+  },
+  {
+    name: 'Doughnut',
+    items: 15,
+    bgColor: 'bg-soft-primary-lightgreen',
+    imgSrc: Doughnut,
+  },
+])
+
+const nextSlide = () => {
+  if (carousel.value) {
+    carousel.value.scrollLeft += 246 // Moves one item forward
+  }
+}
+
+const prevSlide = () => {
+  if (carousel.value) {
+    carousel.value.scrollLeft -= 246 // Moves one item backward
+  }
+}
+</script>
+
+<style>
+/* Hilangkan scrollbar */
+.scrollbar-hide::-webkit-scrollbar {
+  display: none;
+}
+</style>
---
+++ src/components/TrendingComponent.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="flex flex-col gap-10">
+    <div id="text-content" class="flex flex-col gap-2">
+      <h1 class="text-2xl md:text-4xl font-medium text-primary-green">Browse Our Trending</h1>
+      <h1 class="text-2xl md:text-4xl font-medium">Receipt</h1>
+    </div>
+    <div
+      id="trending-menu"
+      class="grid grid-cols-1 md:grid-cols-4 justify-items-stretch gap-6 rounded-2xl"
+    >
+      <div
+        v-for="(item, index) in trending"
+        :key="index"
+        :class="[item.bgColor]"
+        id="card-menu"
+        class="h-[390px] px-6 rounded-2xl w-full bg-none shadow-2xl md:px-8 flex flex-col gap-4 justify-center hover:bg-[url(../assets/bg-main.png)] hover:bg-cover hover:bg-blend-soft-light"
+      >
+        <img :src="item.imgSrc" alt="" class="object-cover bg-cover w-36 h-auto" />
+        <h1 class="text-3xl text-black">{{ item.name }}</h1>
+        <h2 class="text-xl text-gray-400">{{ item.type }}</h2>
+        <img :src="item.stars" alt="" class="object-cover w-20" />
+      </div>
+    </div>
+    <div id="button" class="self-center">
+      <ButtonComponent
+        outlineColor="hover:outline-1"
+        bgColor="bg-primary-green"
+        textColor="text-white"
+        hoverBg="hover:bg-white"
+        hoverOutline="hover:outline-primary-green"
+        hoverText="hover:text-primary-green"
+        >ALL Receipt</ButtonComponent
+      >
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import ButtonComponent from '@/components/ui/ButtonComponent.vue'
+import Four from '@/assets/icons/four-stars.png'
+import Three from '@/assets/icons/three-stars.png'
+import Five from '@/assets/icons/five-stars.png'
+import CupcakeChoco from '@/assets/images/cupcake-choco.png'
+import DonerKebab from '@/assets/images/doner-kebab.png'
+import DoughnutMilk from '@/assets/images/doughnut-milk.png'
+import DoughnutUnicorn from '@/assets/images/dougnut-unicorn.png'
+import KathiKebab from '@/assets/images/kathi-kebab.png'
+import SalmonRoll from '@/assets/images/salmon-roll.png'
+import PizzaMeat from '@/assets/images/pizzameat.png'
+import PizzaPaperoni from '@/assets/images/pizzapaperoni.png'
+
+const trending = ref([
+  {
+    name: 'Pizza Paperoni',
+    type: 'Pizza',
+    bgColor: 'bg-soft-primary-teal',
+    imgSrc: PizzaPaperoni,
+    stars: Four,
+  },
+  {
+    name: 'Pizza Meat',
+    type: 'Pizza',
+    bgColor: 'bg-soft-primary-teal',
+    imgSrc: PizzaMeat,
+    stars: Three,
+  },
+  {
+    name: 'Doner Kebab',
+    type: 'Kebab',
+    bgColor: 'bg-soft-primary-blue',
+    imgSrc: DonerKebab,
+    stars: Five,
+  },
+  {
+    name: 'Salmon Roll',
+    type: 'Salmon',
+    bgColor: 'bg-soft-primary-magenta',
+    imgSrc: SalmonRoll,
+    stars: Four,
+  },
+  {
+    name: 'Cupcake Choco',
+    type: 'Cupcake',
+    bgColor: 'bg-soft-primary-green',
+    imgSrc: CupcakeChoco,
+    stars: Four,
+  },
+  {
+    name: 'Doughnut Milk',
+    type: 'Doughnut',
+    bgColor: 'bg-soft-primary-lightgreen',
+    imgSrc: DoughnutMilk,
+    stars: Five,
+  },
+  {
+    name: 'Doughnut Unicorn',
+    type: 'Doughnut',
+    bgColor: 'bg-soft-primary-lightgreen',
+    imgSrc: DoughnutUnicorn,
+    stars: Four,
+  },
+  {
+    name: 'Kathi Kebab',
+    type: 'Kebab',
+    bgColor: 'bg-soft-primary-blue',
+    imgSrc: KathiKebab,
+    stars: Four,
+  },
+])
+</script>
